058621c43d2d9e63c67f92499268a7c4f05dfb4c
Ivan Strogan <i.strogan@gmail.com>
2026-05-02 12:04:39 -0600

    Rewrite plugin as PHP tar.gz .dlm matching Synology format

---
+++ dlm/search.php
@@ -0,0 +1,61 @@
+<?php
+class SynoDLMSearch1337xJackett {
+    private $jackett_url = 'http://192.168.0.6:9117';
+    private $api_key = '0dhutx0dofdrjcscx29ni6xt8cne5c5t';
+    private $indexer = '1337x';
+
+    public function prepare($curl, $query) {
+        $url = sprintf(
+            '%s/api/v2.0/indexers/%s/results/torznab/?apikey=%s&Query=%s',
+            $this->jackett_url,
+            $this->indexer,
+            $this->api_key,
+            urlencode($query)
+        );
+        curl_setopt($curl, CURLOPT_URL, $url);
+    }
+
+    public function parse($plugin, $response) {
+        $res = 0;
+
+        $xml = simplexml_load_string($response);
+        if (!$xml) return $res;
+
+        $ns = $xml->getNamespaces(true);
+        $torznab_ns = isset($ns['torznab']) ? $ns['torznab'] : 'http://torznab.com/schemas/2015/feed';
+
+        foreach ($xml->channel->item as $item) {
+            $title    = (string)$item->title;
+            $link     = (string)$item->link;
+            $size     = (int)$item->size;
+            $pub_date = (string)$item->pubDate;
+            $guid     = (string)$item->guid;
+
+            $seeds    = 0;
+            $leechs   = 0;
+            $magnet   = '';
+            $category = 'Other';
+
+            foreach ($item->children($torznab_ns) as $attr) {
+                $name  = (string)$attr['name'];
+                $value = (string)$attr['value'];
+                if ($name === 'seeders')   $seeds  = (int)$value;
+                elseif ($name === 'leechers') $leechs = (int)$value;
+                elseif ($name === 'magneturl') $magnet = $value;
+                elseif ($name === 'genre')    $category = $value;
+            }
+
+            $download = $magnet ? $magnet : $link;
+            $hash     = md5($guid . $title);
+            $datetime = $pub_date ? date('Y-m-d H:i', strtotime($pub_date)) : date('Y-m-d H:i');
+
+            if ($title && $download) {
+                $plugin->addResult($title, $download, $size, $datetime, 'https://1337x.to', $hash, $seeds, $leechs, $category);
+                $res++;
+            }
+        }
+
+        return $res;
+    }
+}
+?>
